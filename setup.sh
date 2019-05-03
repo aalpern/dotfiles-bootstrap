02074aac3db98fe27349637d15cac2b3168b1aeb
Adam Alpern <adam.alpern@gmail.com>
2019-05-03 08:01:49 -0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+export EDITOR=emacs
+
+#
+# Shell and SSH 
+#
+
+echo "==> Changing shell to ZSH..."
+chsh -s /bin/zsh
+
+#
+# Core software install
+#
+
+if [ -z `which brew` ]; then
+    echo "==> Installing Homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "==> Skipping brew"
+fi
+
+echo "==> Installing Homebrew utilities..."
+brew install wget tree
+
+echo "==> Installing Homebrew dev tools..."
+brew install git go node source-highlight jq jo protobuf python@3
+
+echo "==> Installing Homebrew virtualization tools..."
+brew cask install virtualbox
+brew install docker docker-machine
+
+if [ ! -d "$HOME/dev/python/bin" ]; then
+    echo "==> Bootstrapping python env..."
+    mkdir -p $HOME/dev
+    python3 -m venv $HOME/dev/python
+else
+    echo "==> Skipping global python env"
+fi
+
+source $HOME/dev/python/bin/activate
+
+if [ -z `which aws` ]; then
+    echo "==> Installing Amazon AWS CLI..."
+    source $HOME/dev/python/bin/activate
+    pip install awscli
+else
+    echo "==> Skipping AWS CLI"
+fi
+
+if [ -z `which homesick` ]; then
+    echo "==> Installing homesick..."
+    sudo gem install homesick
+fi
+
+#
+# Configuration
+#
+
+echo "==> Configuring git..."
+git config --global user.name "Adam Alpern"
+git config --global user.email "adam.alpern@gmail.com"
+git config --global credential.helper osxkeychain
+git config --global color.ui "auto"
+git config --global push.default simple
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+if ! [ -d "$HOME/.homesick/repos/dotfiles" ]; then
+    echo "==> Cloning dotfiles..."
+    homesick clone git@github.com:aalpern/dotfiles.git dotfiles
+    homesick link dotfiles
+else
+    echo "==> Dotfiles already set up"
+fi
+
+#
+# OSX Apps
+#
+
+echo "==> Downloading OS X Apps..."
+mkdir -p $HOME/tmp
+cd $HOME/tmp
+wget https://github.com/MacDownApp/macdown/releases/download/v0.7.1/MacDown.app.zip
+wget https://emacsformacosx.com/emacs-builds/Emacs-26.2-universal.dmg
+wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
+wget https://iterm2.com/downloads/stable/latest -O iterm.zip
+unzip *.zip
+
+open $HOME/tmp
